badb839d29ebcd05b1f4368757cefd96f3c0f04e
ChelesteWang <1581446178@qq.com>
2022-01-25 10:34:44 +0800

    feat: webpack config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        use: [
+          {
+            loader: path.resolve(
+              __dirname,
+              "./loader/add-annotation-loader.js"
+            ),
+            options: {
+              author: "ChelesteWang",
+              date: new Date(),
+            },
+          },
+          {
+            loader: path.resolve(__dirname, "./loader/clear-console-loader.js"),
+          },
+        ],
+      },
+    ],
+  },
+};
